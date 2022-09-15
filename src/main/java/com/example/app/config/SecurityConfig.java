d63c3b5f9d59ce181229f2e4e97a69b1eafdf263
muraoka <ym.animator@gmail.com>
2022-09-15 15:10:49 +0900

    設定ファイルの修正
    ・独自ログインフォームを利用可能に設定
    ・インメモリのユーザーを削除

--- src/main/java/com/example/app/config/SecurityConfig.java
+++ src/main/java/com/example/app/config/SecurityConfig.java
@@ -4,12 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,7 +21,18 @@ public class SecurityConfig {
 			requests.antMatchers("/admin/**").hasRole("ADMIN");
 			requests.anyRequest().authenticated();
 		})
-		.formLogin();
+		.formLogin(form -> {
+			form.loginPage("/login")
+			    .usernameParameter("loginId")
+			    .passwordParameter("loginPass")
+			    .failureForwardUrl("/loginFailure")
+			    .permitAll();
+		})
+		.logout(logout -> {
+			logout.invalidateHttpSession(true)
+			      .logoutSuccessUrl("/logoutDone")
+			      .permitAll();
+		});
 
 		return http.build();
 	}
@@ -34,26 +41,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-	
-	@Bean
-	public UserDetailsManager userDetailsManager() {
-		UserDetails taro = User.withUsername("taro")
-				.password(passwordEncoder().encode("pass"))
-				.roles("NORMAL", "ADMIN")
-				.build();
-		
-		UserDetails hana = User.withUsername("hana")
-				.password(passwordEncoder().encode("pass"))
-				.roles("NORMAL")
-				.build();
-		
-		UserDetails jiro = User.withUsername("jiro")
-				.password(passwordEncoder().encode("pass"))
-				.roles("ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(taro, hana, jiro);
-	}
 
 }
 
